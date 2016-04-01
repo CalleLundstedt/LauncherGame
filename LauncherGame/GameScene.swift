9f5df0732938cdb7d82cbc9c78caa6f4e3068b14
Calle Lundstedt <calle.lundstedt@gmail.com>
2016-04-01 09:11:01 +0200

    Första kommit, fixat pilen

--- LauncherGame/GameScene.swift
+++ LauncherGame/GameScene.swift
@@ -9,37 +9,97 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+    
+    
+    let arrow = SKSpriteNode(imageNamed: "arrow")
+    let playableRect: CGRect
+    var arrowDefaultWidth: CGFloat = 0.0
+    var lastUpdateTime: NSTimeInterval = 0.0, dt: NSTimeInterval = 0
+    
+    override init(size: CGSize) {
+        let maxAspectRatio:CGFloat = 16.0/9.0
+        let playableHeight = size.width / maxAspectRatio
+        let playableMargin = (size.height-playableHeight)/2.0
+        playableRect = CGRect(x: 0, y: playableMargin, width: size.width, height: playableHeight)
         
-        self.addChild(myLabel)
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func didMoveToView(view: SKView) {
+        arrow.anchorPoint = CGPointMake(0,0.5);
+        arrow.position = CGPoint(x: CGRectGetMinX(playableRect), y: CGRectGetMinY(playableRect))
+        arrowDefaultWidth = arrow.size.width
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        let touch = touches.first! as UITouch
+        let touchLocation = touch.locationInNode(self)
+        touchStart(touchLocation)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        let touchLocation = touch.locationInNode(self)
+        touchMoved(touchLocation)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        touchStopped()
     }
    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+
+    }
+    
+    func touchStart(touchPoint: CGPoint) {
+        let delta = arrow.position-touchPoint
+        var angle = delta.angle + π
+        
+        if(angle < 0) {
+            angle += 2*π
+        }
+        
+        arrow.zRotation = angle
+        if(touchPoint.length() < 500) {
+            let scale = touchPoint.length()/arrowDefaultWidth
+            let scaleAction = SKAction.scaleTo(scale, duration: 0)
+            arrow.runAction(scaleAction)
+        }
+        addChild(arrow)
+        
+    }
+    
+    func touchMoved(touchPoint: CGPoint) {
+        let delta = arrow.position-touchPoint
+        var angle = delta.angle + π
+        
+        if(angle < 0) {
+            angle += 2*π
+        }
+        
+        let rotateAction = SKAction.rotateToAngle(angle, duration: 0)
+        arrow.runAction(rotateAction)
+        
+        if(touchPoint.length() < 500) {
+            let scale = touchPoint.length()/arrowDefaultWidth
+            print(touchPoint.length())
+            let scaleAction = SKAction.scaleTo(scale, duration: 0)
+            arrow.runAction(scaleAction)
+        }
+    }
+    
+    func touchStopped() {
+        arrow.removeFromParent()
     }
 }
---
+++ LauncherGame/Utils.swift
@@ -0,0 +1,47 @@
+//
+//  Utils.swift
+//  LauncherGame
+//
+//  Created by Carl Lundstedt on 28/03/16.
+//  Copyright © 2016 Carl Lundstedt. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+let π = CGFloat(M_PI)
+
+func shortestAngleBetween(angle1: CGFloat, angle2: CGFloat) -> CGFloat {
+    let twoπ = π*2
+    
+    var angle = (angle2-angle1) % twoπ
+    if(angle >= π) {
+        angle = angle - twoπ
+    }
+    if(angle <= -π) {
+        angle = angle + twoπ
+    }
+    return angle
+}
+
+func / (left: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x/scalar, y: left.y/scalar)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x-right.x, y: left.y-right.y)
+}
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+    
+    var angle: CGFloat {
+        return atan2(y,x)
+    }
+}
